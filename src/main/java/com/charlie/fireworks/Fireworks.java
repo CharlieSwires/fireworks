5fd05806432927f8ba0bf3d0c7e77755e1400696
CharlieSwires <charlieswires@gmail.com>
2020-12-29 22:31:54 +0000

    Added manifest changed to 1.8 JDK

--- src/main/java/com/charlie/fireworks/Fireworks.java
+++ src/main/java/com/charlie/fireworks/Fireworks.java
@@ -6,8 +6,6 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
@@ -18,8 +16,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 
 public class Fireworks extends JPanel{
